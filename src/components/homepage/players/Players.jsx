890bdd14ce901a610427c5c786cb80ca006eb626
raf0670 <swagbondhu@gmail.com>
2026-03-26 10:50:32 +0600

    state added

--- src/components/homepage/players/Players.jsx
+++ src/components/homepage/players/Players.jsx
@@ -1,12 +1,25 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './availablePlayers/AvailablePlayers';
+import SelectedPlayers from './selectedPlayers/SelectedPlayers';
 
-const Players = ({playersPromise}) => {
+const Players = ({ playersPromise }) => {
     const players = use(playersPromise);
-    // console.log(players);
+
+    const [selectedType, setSelectedType] = useState("available");
+
     return (
         <div className=''>
-            <AvailablePlayers players={players}></AvailablePlayers>
+
+            <div className='flex justify-between my-12 items-center'>
+                <h2 className='font-bold text-2xl'>{selectedType == "available" ? `Available Player` : `Selected (0/6)`}</h2>
+
+                <div>
+                    <button onClick={() => setSelectedType("available")} className={`${selectedType == "available" ? `bg-[#E7FE29] text-black` : `bg-white text-gray-500`} p-1 px-5 font-semibold outline outline-gray-100 rounded-r-none rounded-xl hover:scale-105 duration-500`}>Available</button>
+                    <button onClick={() => setSelectedType("selected")} className={`${selectedType == "selected" ? `bg-[#E7FE29] text-black` : `bg-white text-gray-500`} p-1 px-5 font-semibold outline outline-gray-100 rounded-l-none rounded-xl hover:scale-105 duration-500`}>Selected (0)</button>
+                </div>
+            </div>
+
+            {selectedType == "available" ? <AvailablePlayers players={players}></AvailablePlayers> : <SelectedPlayers></SelectedPlayers>}
         </div>
     );
 };
